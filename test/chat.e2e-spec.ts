6906c78b663ae643b8f68d6b008d8319c52136ea
Alisher Nuraliyev <alisher.nuraliyev@student.howest.be>
2026-01-19 00:24:17 +0100

    fixed issues in chat test

--- test/chat.e2e-spec.ts
+++ test/chat.e2e-spec.ts
@@ -100,27 +100,21 @@ describe('ChatGateway (e2e)', () => {
       auth: { token: `Bearer ${firstUsertoken}` },
     });
 
-    // 1. Ждем подключения
     await new Promise((resolve) => clientSocket.on('connect', resolve));
 
-    // 2. Входим в комнату
     clientSocket.emit('joinCommunity', communityId);
 
-    // Даем микро-паузу, чтобы сервер успел обработать join
     await new Promise((resolve) => setTimeout(resolve, 50));
 
-    // 3. Готовим обещание (Promise), которое выполнится, когда придет сообщение
     const messageReceived = new Promise((resolve) => {
       clientSocket.on('newMessage', (message) => resolve(message));
     });
 
-    // 4. Отправляем сообщение
     clientSocket.emit('messageToCommunity', {
       communityId,
       content: 'Hello Community',
     });
 
-    // 5. Ждем получения сообщения и проверяем его
     const message: any = await messageReceived;
     expect(message.content).toBe('Hello Community');
     expect(message.communityId).toBe(communityId);
@@ -135,7 +129,6 @@ describe('ChatGateway (e2e)', () => {
       auth: { token: `Bearer ${secondUsertoken}` },
     });
 
-    // 1. Ждем, пока оба сокета подключатся
     await Promise.all([
       new Promise<void>((resolve) =>
         clientSocket.on('connect', () => resolve()),
@@ -145,18 +138,15 @@ describe('ChatGateway (e2e)', () => {
       ),
     ]);
 
-    // 2. Готовим обещание для ПОЛУЧАТЕЛЯ (второго пользователя)
     const privateMessageReceived = new Promise((resolve) => {
       secondClientSocket.on('newPrivateMessage', (message) => resolve(message));
     });
 
-    // 3. Отправитель (первый пользователь) шлет сообщение
     clientSocket.emit('messageToPrivate', {
       receiverId: userId,
       content: 'Hello Private Message',
     });
 
-    // 4. Ждем получения сообщения вторым пользователем
     const message: any = await privateMessageReceived;
     expect(message.content).toBe('Hello Private Message');
   });
